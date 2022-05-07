b86cddaf63bbb8214cb0c112ef29f5f3293457fe
molly.im <seulali79@gmail.com>
2022-05-07 15:57:15 +0900

    변수 초기화

--- app/src/main/java/com/example/kotlinbasic/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasic/MainActivity.kt
@@ -12,6 +12,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//        문법 연습 (변수)
+
+//        변수 만들기
+
+        val myName = "몰리" // 변수 생성과 대입을 동시에 하는 것을 '초기화'라고 함 (변수의 초기화)
+
+        Log.d("변수값",myName)
+
+
         clickBtn.setOnClickListener {
 
 //            이 {} 안 코드만, 버튼 눌렸을 때 실행됩니다.
