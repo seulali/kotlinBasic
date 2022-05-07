f67b1c783b2113308fc7b0f345f653821d5e0c05
molly.im <seulali79@gmail.com>
2022-05-07 15:14:33 +0900

    로그 찍어보기

--- app/src/main/java/com/example/kotlinbasic/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasic/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.kotlinbasic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +11,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        clickBtn.setOnClickListener {
 
-    }
+//            이 {} 안 코드만, 버튼 눌렸을 때 실행됩니다.
+
+            Log.d("메인화면로그","클릭용 버튼이 눌림")
+        }
+
+   }
 
 }
